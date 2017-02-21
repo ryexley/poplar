ccc27e951313531fbc2a31d15b87dbaba0356c17
Bob Yexley <bob@yexley.net>
2017-02-20 23:14:06 -0500

    Basic UI layout and wiring

--- app/js/app.js
+++ app/js/app.js
@@ -1,6 +1,50 @@
+const { template } = require( "lodash" );
+const Client = require( "electron-rpc/client" );
+const client = new Client();
+
+function quit() {
+    client.request( "quit" );
+}
+
+function toggleDeviceState( e ) {
+    const toggle = e.currentTarget;
+    const state = toggle.getAttribute( "data-state" );
+    const icon = toggle.querySelector( "i" );
+
+    if ( state === "off" ) {
+        toggle.setAttribute( "data-state", "on" );
+        icon.classList.remove( "fa-toggle-off" );
+        icon.classList.add( "fa-toggle-on" );
+    } else {
+        toggle.setAttribute( "data-state", "off" );
+        icon.classList.remove( "fa-toggle-on" );
+        icon.classList.add( "fa-toggle-off" );
+    }
+}
+
+function registerEvents() {
+    document.getElementById( "quit-button" ).addEventListener( "click", quit );
+    Array.from( document.querySelectorAll( ".device-stateToggle" ) ).forEach( toggle => { toggle.addEventListener( "click", toggleDeviceState ); } );
+    // console.log( document.querySelectorAll( ".device-stateToggle" ).length );
+}
+
+function renderDevice( { friendlyName } ) {
+    const deviceControlTemplate = template( [
+        "<div class='device'>",
+        "   <span class='device-name'>${ friendlyName }</span>",
+        "   <span class='device-stateToggle' data-state='off'><i class='fa fa-toggle-off' aria-hidden='true'></i></span>",
+        "</div>"
+    ].join( "" ) );
+
+    const deviceControlEl = deviceControlTemplate( { friendlyName } );
+    document.getElementById( "root" ).innerHTML += deviceControlEl;
+}
+
 document.addEventListener( "DOMContentLoaded", function() {
-	// alert( "Wemo Menubar Controller" );
-	// document.querySelector( "#root" ).addEventListener( "click", function() {
-	// 	app.hide();
-	// } );
+    // alert( "Wemo Menubar Controller" );
+    // document.querySelector( "#root" ).addEventListener( "click", function() {
+    //  app.hide();
+    // } );
+    renderDevice( { friendlyName: "In a meeting" } );
+    registerEvents();
 } );
--- index.js
+++ index.js
@@ -1,10 +1,24 @@
+const Server = require( "electron-rpc/server" );
 const Menubar = require( "menubar" );
 
 const mb = new Menubar( {
-	dir: "./app",
-	height: 125,
-	width: 250
+    dir: "./app",
+    height: 125,
+    width: 250,
+    alwaysOnTop: true
 } );
 
-mb.on( "after-hide", () => { mb.app.hide(); } );
+const app = new Server();
 
+// mb.on( "after-hide", () => { mb.app.hide(); } );
+
+mb.on( "after-create-window", () => {
+    mb.window.setResizable( false );
+    mb.window.openDevTools();
+} );
+
+mb.on( "ready", () => {
+    app.on( "quit", () => {
+        mb.app.quit();
+    } );
+} );
